f2c13dac14c0f241e9be44946c53bc97bd159e6a
Bowen <bx4y4@g.ucla.edu>
2018-02-04 18:03:50 -0500

    adding hibernateProj

---
+++ Hibernate/src/main/java/com/Leo/JDBC/TestJDBC.java
@@ -0,0 +1,21 @@
+package com.Leo.JDBC;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class TestJDBC {
+	public static void main(String[] args) {
+		
+		String jdbcUrl = "jdbc:mysql://localhost:3306/hb_student_tracker?useSSL=false";
+		String user = "hbstudent";
+		String pass = "hbstudent";
+		try {
+			System.out.println("Connecting to databse: "+jdbcUrl);
+			Connection myConn  = DriverManager.getConnection(jdbcUrl, user, pass);
+			System.out.println("Connection successful!");
+		}
+		catch(Exception exc) {
+			exc.printStackTrace();
+		}
+	}
+}
