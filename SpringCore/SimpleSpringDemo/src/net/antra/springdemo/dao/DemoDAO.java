cc5ed4dd8e2d90d8195da69d98708e72a070ec0a
Bowen <bx4y4@g.ucla.edu>
2018-02-03 22:50:41 -0500

    Deleting SpringCore

--- SpringCore/SimpleSpringDemo/src/net/antra/springdemo/dao/DemoDAO.java
+++
@@ -1,6 +0,0 @@
-package net.antra.springdemo.dao;
-
-public interface DemoDAO { 
-	public void doSomething();
-	public void saveDemo();
-}
--- SpringCore/SimpleSpringDemo/src/net/antra/springdemo/service/DemoService.java
+++
@@ -1,5 +0,0 @@
-package net.antra.springdemo.service;
-
-public interface DemoService {
-	public void doSomeThing();
-}
